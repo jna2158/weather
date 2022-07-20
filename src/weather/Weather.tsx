f885d3593bd7f79c74c23ed4b475d661eb37ab19
jna2158 <jna2158@deepnoid.com>
2022-07-20 16:25:09 +0900

    fix: api-key

--- src/weather/Weather.tsx
+++ src/weather/Weather.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import axios from "axios";
-const API_KEY = "a83ffb3c067c0596b9efc118be645a47";
 
 const Weather = () => {
   useEffect(() => {
@@ -9,16 +8,16 @@ const Weather = () => {
 
   const getWeather = async () => {
     const data = "Seoul";
-    let reqUrl = `http://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${API_KEY}`;
+    // let reqUrl = `http://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${API_KEY}`;
 
-    await axios
-      .get(reqUrl)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // await axios
+    //   .get(reqUrl)
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   };
 
   return <div>weather</div>;
